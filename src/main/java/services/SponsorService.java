1052b12ec02bed292c7c887e44f5f1141bf5a4b3
luigilgue <luigilgue@alum.us.es>
2019-03-14 15:03:29 +0100

    Services: Sponsor y Sponsorship

---
+++ src/main/java/services/SponsorService.java
@@ -0,0 +1,173 @@
+
+package services;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Assert;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.Validator;
+
+import repositories.SponsorRepository;
+import security.Authority;
+import security.UserAccount;
+import domain.Box;
+import domain.SocialProfile;
+import domain.Sponsor;
+import forms.FormObjectSponsor;
+
+@Service
+@Transactional
+public class SponsorService {
+
+	//Managed repository ---------------------------------
+
+	@Autowired
+	private SponsorRepository	SponsorRepository;
+
+	//Supporting services --------------------------------
+
+	@Autowired
+	private BoxService			boxService;
+
+	@Autowired
+	private ActorService		actorService;
+
+	@Autowired
+	private Validator			validator;
+
+
+	//Simple CRUD Methods --------------------------------
+
+	public Sponsor create() {
+		final Authority a = new Authority();
+		a.setAuthority(Authority.SPONSOR);
+		final UserAccount account = new UserAccount();
+		account.setAuthorities(Arrays.asList(a));
+		account.setBanned(false);
+
+		final Sponsor Sponsor = new Sponsor();
+		Sponsor.setSpammer(false);
+		Sponsor.setSocialProfiles(new ArrayList<SocialProfile>());
+		Sponsor.setUserAccount(account);
+		Sponsor.setBoxes(new ArrayList<Box>());
+
+		return Sponsor;
+	}
+
+	public Collection<Sponsor> findAll() {
+		return this.SponsorRepository.findAll();
+	}
+
+	public Sponsor findOne(final int id) {
+		Assert.notNull(id);
+
+		return this.SponsorRepository.findOne(id);
+	}
+
+	public Sponsor save(final Sponsor Sponsor) {
+		Assert.notNull(Sponsor);
+		Sponsor saved2;
+
+		//Assertion that the email is valid according to the checkAdminEmail method.
+		Assert.isTrue(this.actorService.checkUserEmail(Sponsor.getEmail()));
+
+		//Assertion to check that the address isn't just a white space.
+		Assert.isTrue(this.actorService.checkAddress(Sponsor.getAddress()));
+
+		//Assertion that the phone is valid according to the checkPhone method.
+		Assert.isTrue(this.actorService.checkPhone(Sponsor.getPhone()));
+
+		//Checking if the actor is bannable according to the "bannableActors" query.
+		if (this.actorService.isBannable(Sponsor) == true)
+			Sponsor.setSpammer(true);
+
+		if (Sponsor.getId() != 0) {
+			Assert.isTrue(this.actorService.findByPrincipal().getId() == Sponsor.getId());
+			saved2 = this.SponsorRepository.save(Sponsor);
+		} else {
+			final Sponsor saved = this.SponsorRepository.save(Sponsor);
+			this.actorService.hashPassword(saved);
+			saved.setBoxes(this.boxService.generateDefaultFolders(saved));
+			saved2 = this.SponsorRepository.save(saved);
+		}
+
+		return saved2;
+	}
+
+	public void delete(final Sponsor Sponsor) {
+		Assert.notNull(Sponsor);
+
+		//Assertion that the user deleting this Sponsor has the correct privilege.
+		Assert.isTrue(this.actorService.findByPrincipal().getId() == Sponsor.getId());
+
+		this.SponsorRepository.delete(Sponsor);
+	}
+
+	//Reconstruct
+
+	public Sponsor reconstruct(final FormObjectSponsor fos, final BindingResult binding) {
+		final Sponsor result = this.create();
+
+		Assert.isTrue(fos.getAcceptedTerms());
+		Assert.isTrue(fos.getPassword().equals(fos.getSecondPassword()));
+
+		result.setName(fos.getName());
+		result.setMiddleName(fos.getMiddleName());
+		result.setSurname(fos.getSurname());
+		result.setPhoto(fos.getPhoto());
+		result.setEmail(fos.getEmail());
+		result.setPhone(fos.getPhone());
+		result.setAddress(fos.getAddress());
+		result.getUserAccount().setUsername(fos.getUsername());
+		result.getUserAccount().setPassword(fos.getPassword());
+
+		this.validator.validate(result, binding);
+
+		//Assertion that the email is valid according to the checkAdminEmail method.
+		Assert.isTrue(this.actorService.checkUserEmail(result.getEmail()));
+
+		//Assertion to check that the address isn't just a white space.
+		Assert.isTrue(this.actorService.checkAddress(result.getAddress()));
+
+		//Assertion that the phone is valid according to the checkPhone method.
+		Assert.isTrue(this.actorService.checkPhone(result.getPhone()));
+
+		return result;
+
+	}
+
+	public Sponsor reconstructPruned(final Sponsor Sponsor, final BindingResult binding) {
+		Sponsor result;
+
+		result = this.SponsorRepository.findOne(Sponsor.getId());
+
+		result.setName(Sponsor.getName());
+		result.setMiddleName(Sponsor.getMiddleName());
+		result.setSurname(Sponsor.getSurname());
+		result.setPhoto(Sponsor.getPhoto());
+		result.setEmail(Sponsor.getEmail());
+		result.setPhone(Sponsor.getPhone());
+		result.setAddress(Sponsor.getAddress());
+
+		this.validator.validate(result, binding);
+
+		Assert.isTrue(this.actorService.findByPrincipal().getId() == result.getId());
+
+		//Assertion that the email is valid according to the checkAdminEmail method.
+		Assert.isTrue(this.actorService.checkUserEmail(result.getEmail()));
+
+		//Assertion to check that the address isn't just a white space.
+		Assert.isTrue(this.actorService.checkAddress(result.getAddress()));
+
+		//Assertion that the phone is valid according to the checkPhone method.
+		Assert.isTrue(this.actorService.checkPhone(result.getPhone()));
+
+		return result;
+
+	}
+}
---
+++ src/main/java/services/SponsorshipService.java
@@ -0,0 +1,147 @@
+
+package services;
+
+import java.util.Calendar;
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.Assert;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.Validator;
+
+import repositories.SponsorshipRepository;
+import domain.CreditCard;
+import domain.Parade;
+import domain.Sponsor;
+import domain.Sponsorship;
+
+public class SponsorshipService {
+
+	// Managed repository
+	@Autowired
+	private SponsorshipRepository	sponsorshipRepository;
+
+	// Supporting service
+
+	@Autowired
+	private ActorService			actorService;
+
+	@Autowired
+	private Validator				validator;
+
+
+	// Simple CRUD methods
+
+	public Sponsorship create() {
+		final Sponsorship ss = new Sponsorship();
+		final Sponsor s = (Sponsor) this.actorService.findByPrincipal();
+		ss.setSponsor(s);
+		ss.setCreditCard(new CreditCard());
+		ss.setParade(new Parade());
+		ss.setIsActive(true);
+		return ss;
+	}
+
+	public Sponsorship findOne(final int id) {
+		Assert.notNull(id);
+
+		return this.sponsorshipRepository.findOne(id);
+	}
+
+	public Collection<Sponsorship> findAll() {
+		return this.sponsorshipRepository.findAll();
+	}
+
+	public Sponsorship save(final Sponsorship ss) {
+		Assert.notNull(ss);
+
+		final int year = Calendar.getInstance().get(Calendar.YEAR);
+		final int month = Calendar.getInstance().get(Calendar.MONTH);
+
+		//Assertion that the user modifying this sponsorship has the correct privilege.
+		Assert.isTrue(this.actorService.findByPrincipal().getId() == ss.getSponsor().getId());
+
+		//Assertion to make sure that the credit card has a valid expiration date.
+		if (ss.getCreditCard() != null) {
+			Assert.isTrue(ss.getCreditCard().getExpYear() >= year);
+
+			if (ss.getCreditCard().getExpYear() == year)
+				Assert.isTrue(ss.getCreditCard().getExpMonth() >= month);
+		}
+
+		//Assertion that the parade is accepted
+		Assert.isTrue(ss.getParade().getParadeStatus().equals("ACCEPTED"));
+
+		final Sponsorship saved = this.sponsorshipRepository.save(ss);
+		return saved;
+	}
+
+	public void delete(final Sponsorship ss) {
+		Assert.notNull(ss);
+
+		//Assertion that the user deleting this sponsorship has the correct privilege.
+		Assert.isTrue(this.actorService.findByPrincipal().getId() == ss.getSponsor().getId());
+
+		this.sponsorshipRepository.delete(ss);
+	}
+
+	public void remove(final Sponsorship ss) {
+		Assert.notNull(ss);
+
+		//Assertion that the user removing this sponsorship has the correct privilege.
+		Assert.isTrue(this.actorService.findByPrincipal().getId() == ss.getSponsor().getId());
+		//Assertion that the sponsorship is active
+		Assert.isTrue(ss.getIsActive());
+
+		ss.setIsActive(false);
+
+		this.sponsorshipRepository.save(ss);
+	}
+
+	public void activate(final Sponsorship ss) {
+		Assert.notNull(ss);
+
+		//Assertion that the user activating this sponsorship has the correct privilege.
+		Assert.isTrue(this.actorService.findByPrincipal().getId() == ss.getSponsor().getId());
+		//Assertion that the sponsorship is deactive
+		Assert.isTrue(!(ss.getIsActive()));
+
+		ss.setIsActive(false);
+
+		this.sponsorshipRepository.save(ss);
+	}
+
+	public Sponsorship reconstruct(final Sponsorship ss, final BindingResult binding) {
+		Sponsorship result;
+
+		final int year = Calendar.getInstance().get(Calendar.YEAR);
+		final int month = Calendar.getInstance().get(Calendar.MONTH);
+
+		if (ss.getId() == 0)
+			result = this.create();
+		else
+			result = this.sponsorshipRepository.findOne(ss.getId());
+		result.setBanner(ss.getBanner());
+		result.setTargetURL(ss.getTargetURL());
+		result.setCreditCard(ss.getCreditCard());
+		result.setIsActive(ss.getIsActive());
+
+		this.validator.validate(result, binding);
+
+		//Assertion that the user modifying this sponsorship has the correct privilege.
+		Assert.isTrue(this.actorService.findByPrincipal().getId() == result.getSponsor().getId());
+
+		//Assertion to make sure that the credit card has a valid expiration date.
+		if (ss.getCreditCard() != null) {
+			Assert.isTrue(result.getCreditCard().getExpYear() >= year);
+
+			if (ss.getCreditCard().getExpYear() == year)
+				Assert.isTrue(result.getCreditCard().getExpMonth() >= month);
+		}
+
+		//Assertion that the parade is accepted
+		Assert.isTrue(result.getParade().getParadeStatus().equals("ACCEPTED"));
+
+		return ss;
+	}
+}
