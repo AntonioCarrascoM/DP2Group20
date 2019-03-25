e604d62e64aa7275ead4cba330e977f284231939
Sermor <sermor95@gmail.com>
2019-03-25 22:06:25 +0100

    Check requirements
    Check requirements until lvl B completed

--- src/main/java/repositories/ChapterRepository.java
+++ src/main/java/repositories/ChapterRepository.java
@@ -14,11 +14,11 @@ public interface ChapterRepository extends JpaRepository<Chapter, Integer> {
 
 	//The average, the minimum, the maximum, and the standard deviation of the
 	//number of parades co-ordinated by the chapters
-	@Query("select avg(b.parades.size), min(b.parades.size), max(b.parades.size), stddev(b.parades.size) from Chapter c join c.area a join a.brotherhoods b")
+	@Query("select avg((select count(p) from Parade p join p.brotherhood b where b.area.id=a.id)*1.), min((select count(p) from Parade p join p.brotherhood b where b.area.id=a.id)*1.), max((select count(p) from Parade p join p.brotherhood b where b.area.id=a.id)*1.), stddev((select count(p) from Parade p join p.brotherhood b where b.area.id=a.id)*1.) from Chapter c join c.area a")
 	Double[] avgMinMaxStddevParadesCoordinatedByChapter();
 
 	//The chapters that co-ordinate at least 10% more parades than the average
-	@Query("select c1.name from Chapter c1 join c1.area a1 join a1.brotherhoods b1 where (b1.parades.size)*1.>(select avg(b.parades.size) from Chapter c join c.area a join a.brotherhoods b)*1.1")
+	@Query("select c1.name from Chapter c1 join c1.area a1 where ((select count(p) from Parade p join p.brotherhood b where b.area.id=a1.id)*1.) > (select avg((select count(p) from Parade p join p.brotherhood b where b.area.id=a.id)*1.1) from Chapter c join c.area a)")
 	Collection<String> chaptersWith10PerCentParadesCoordinateThanAvg();
 
 	//Retrieves the chapter associated with a certain area.
@@ -26,3 +26,5 @@ public interface ChapterRepository extends JpaRepository<Chapter, Integer> {
 	Chapter getChapterForArea(int id);
 
 }
+
+//select c1.name from Chapter c1 join c1.area a1 where ((select count(p) from Parade p join p.brotherhood b where b.area.id=a1.id)*1.) > (select avg((select count(p) from Parade p join p.brotherhood b where b.area.id=a.id)*1.1) from Chapter c join c.area a);
